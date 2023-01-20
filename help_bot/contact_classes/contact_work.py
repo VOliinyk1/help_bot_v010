da9ffe5a5efacdf07112a287546cfc124718ce89
VOliinyk1 <vov.25.98@gmail.com>
2023-01-20 13:30:57 +0200

    file upd

---
+++ help_bot/contact_classes/contact_work.py
@@ -0,0 +1,159 @@
+from help_bot.book import Book
+from contact_classes.fields import Phone, Email, Address, Birthday
+from contact_classes.record import Record
+from help_bot.user_work_abstract import UserWork
+
+class WorkContact(UserWork):
+
+    def __init__(self, path):
+
+        self.contacts_book = Book(path)
+        try:
+            print(self.contacts_book.load_from_file())
+        except FileNotFoundError:
+            self.contacts_book.save_to_file()
+
+    def save_to_file(self):
+        return self.contacts_book.save_to_file()
+
+    def load_from_file(self):
+        return self.contacts_book.load_from_file()
+
+    def create(self, name: str, *_) -> str:
+        """Створюємо новий запис в книгу, якщо запису з таким ім'ям не існує."""
+        if not name:
+            raise ValueError("You can't create empty contact.")
+        if name in self.contacts_book:
+            return f"Contact with name '{name} already exist."
+        else:
+            self.contacts_book[name] = Record(name)
+            return f"Contact with name {name} successfully created."
+
+    def show_all(self, *_) -> list:
+        """Створює рядок з інформацією про кожен контакт"""
+        rec_info = []
+        for record in self.contacts_book.values():
+            rec_info.append(str(record))
+        return rec_info if rec_info else "ContactBook is empty."
+
+    def show_one(self, name: str, *_) -> str:
+        """Виводить інформацію про один конкретний контакт"""
+        if not name:
+            raise ValueError("You can't search empty contact-name.")
+        return f"{str(self.contacts_book[name]) if name in self.contacts_book else f'Contact {name} is not found.'}"
+
+    def show_page(self, number_of_contacts: str, *_) -> list:
+        """Ітеруємось по записам і формуєм сторінку з контактами по number_of_contacts штук на сторінку"""
+        if number_of_contacts:
+            n = int(number_of_contacts)
+        else:
+            n = 5
+        result = []
+        for page in self.contacts_book.iterator(n):
+            result_str = []
+            for record in page:
+                result_str.append(str(self.contacts_book[record]))
+            result.extend(["Page Start", *result_str, "Page End"])
+        return result
+
+    def delete_all(self, *_):
+        """Видаляє ВСІ записи з контактної книги"""
+        answer = input(
+            f"You about to delete all records in book. You sure? Y/N: ")
+        if answer == 'Y':
+            self.contacts_book.data = {}
+            return 'Contacts book now clean'
+        else:
+            return 'Not deleted'
+
+    def delete_one(self, name: str, *_):
+        """Видаляє один конкретний контакт з книги"""
+        if not name:
+            raise ValueError("You can't delete empty contact.")
+        if name in self.contacts_book:
+            del self.contacts_book[name]
+            return f"Contact {name} is deleted"
+        else:
+            return f"Contact {name} is not in book"
+
+    def add_values(self, name: str, args: list):
+        """Додає інформацію до конкретного контактну у вказане поле"""
+        try:
+            field = args[0]
+            information = args[1:]
+        except IndexError:
+            return "You can't add to contact empty information."
+        if not field or not information:
+            return "You can't add to contact empty information."
+        records_fields_methods = {'phones': self.contacts_book[name].add_phone,
+                                  'emails': self.contacts_book[name].add_email,
+                                  'addresses': self.contacts_book[name].set_address,
+                                  'birthday': self.contacts_book[name].set_birthday}
+        if field not in records_fields_methods:
+            return f"Wrong field '{field}'."
+        return records_fields_methods[field](" ".join(information))
+
+    def search_in(self, search_data: str, *_) -> list:
+        """Пошук заданого фрагмента у контактах"""
+        if not search_data:
+            raise ValueError("Empty search information")
+        result = []
+        for value in self.contacts_book.values():
+            search_target = [value.name.value,
+                             *[phone.value for phone in value.phones if isinstance(phone, Phone)],
+                             *[email.value for email in value.emails if isinstance(email, Email)],
+                             *[value.birthday.value if isinstance(value.birthday, Birthday) else None]]
+            if isinstance(value.addresses, Address):
+                search_target.extend(value.addresses.value.split())
+            if search_data in (search_target):
+                result.append(value)
+        return result
+
+    def edit_information(self, name: str, args: list) -> str:
+        """Редагує інформацію у вказаному полі вказаного контакт. Command може приймати два значення - change & del. Change відповідає за
+        заміну інформації. Del - видаляє її."""
+
+        try:
+            command, field, values = args[0], args[1], [args[2], *args[3:]]
+        except IndexError:
+            return "You can't edit contact with empty information."
+        if name in self.contacts_book:
+            return self.contacts_book[name].edit_information_contact(command, field, values)
+        else:
+            return "This contact doesn't exist!"
+
+    def show_nearest_birthdays(self, days: str, *_) -> list:
+        """Показує у кого з контактів відбудеться день народження протягом найблищчих days днів"""
+        try:
+            n = int(days)
+        except ValueError:
+            n = 5
+
+        n_days_birthday = []
+        for contact in self.contacts_book.values():
+            if isinstance(contact.birthday, Birthday) and contact.days_to_birthday() <= n:
+                n_days_birthday.append(f"Days to {contact.name.value}'s birthday : {contact.days_to_birthday()} days \n")
+        return n_days_birthday if n_days_birthday else f'No birthdays in nearest {n} days'
+
+    def days_to_birthday_for_one(self, name: str, *_) -> str:
+        """Показує скільки днів до дня народження конкретного контакту"""
+        return f"Days to birthday for {name} = {self.contacts_book.data[name].days_to_birthday()}"
+
+    def days_to_birthday_for_all(self, *_) -> list:
+        """Показує скільки днів до дня народження всіх контактів"""
+        birthdays = []
+        for contact in self.contacts_book.values():
+            birthdays.append(f"Days to {contact.name.value}'s birthday: {contact.days_to_birthday()}")
+        return birthdays if birthdays else 'There is no birthdays in contacts'
+
+    def edit_name(self, name: str, args: list) -> str:
+        """Змінює ім'я запису. Змінює як ім'я-ключ контакту, так і в самому контакті"""
+        try:
+            new_name = args[0]
+        except IndexError:
+            raise IndexError("You can't edit name with empty information.")
+        record: Record = self.contacts_book[name]
+        record.name.value = new_name
+        self.contacts_book[new_name] = record
+        del self.contacts_book[name]
+        return f"{name}'s name has been changed to {new_name}"
---
+++ help_bot/note_classes/note_work.py
@@ -0,0 +1,134 @@
+from help_bot.book import Book
+from note_classes.note import Note
+from help_bot.user_work_abstract import UserWork
+
+
+class WorkNote(UserWork):
+
+    def __init__(self, path):
+        self.note_book = Book(path)
+        try:
+            print(self.note_book.load_from_file())
+        except FileNotFoundError:
+            self.note_book.save_to_file()
+
+    def create(self, name: str, info: list) -> str:
+        """Створює нову нотатку у книзі, якщо нотатки з таким ім'ям ще не існує"""
+        if name in self.note_book:
+            return f"Note with name '{name} already exist."
+        elif not name or not info:
+            raise ValueError("You can't create empty note.")
+        else:
+            tags = []
+            for word in info:
+                # Шукаємо теги в тексті
+                if word.startswith('#'):
+                    tags.append(word)
+            text = " ".join(info)
+            self.note_book[name] = Note(name, tags, text)
+            return f"Note with name {name} successfully created."
+
+    def show_all(self, *_):
+        """Показує всі нотатки"""
+        all_notes = []
+        for note in self.note_book.values():
+            all_notes.append(str(note))
+        return all_notes if all_notes else 'NoteBook is empty.'
+
+    def show_one(self, name: str, *_):
+        """Показує одну конкретну нотатку"""
+        return str(self.note_book[name])
+
+    def show_page(self, number_of_notes: str, *_) -> list:
+        """Ітеруємось по записам і формуєм рядок з нотатками по number_of_notes штук на сторінку"""
+        if number_of_notes:
+            n = int(number_of_notes)
+        else:
+            n = 5
+        result = []
+        for page in self.note_book.iterator(int(n)):
+            result_str = []
+            for record in page:
+                result_str.append(str(self.note_book[record]))
+            result.extend(["Page Start", *result_str, "Page End"])
+        return result
+
+    def delete_all(self, *_):
+        """Видаляє всі нотатки з книги"""
+        answer = input("You about to delete all notes in notebook. You sure? Y/N: ")
+        if answer == 'Y':
+            self.note_book.data = {}
+            return "Note book now clean"
+        else:
+            return "Not deleted"
+
+    def delete_one(self, name: str, *_):
+        """Видаляє одну конкретну нотатку з книги"""
+        if name in self.note_book.data:
+            del self.note_book.data[name]
+            return f"Note {name} is deleted"
+        else:
+            return f"Note {name} is not in notebook"
+
+    def save_to_file(self):
+        return self.note_book.save_to_file()
+
+    def load_from_file(self):
+        return self.note_book.load_from_file()
+
+    def edit_information(self, name: str, values: list):
+        """Редагує інформацію в нотатку. Через те що немає можливості без графічного інтерфейсу зробити зручне редагування в прямому сенсі -
+        просто видаляє всі поля окрім імені і додає нову інфу"""
+        if not name or not values:
+            raise IndexError("You can't edit note without new information.")
+        note: Note = self.note_book.data[name]
+        if values[0] == 'text':
+            note.clear_text()
+            return note.add_to_note(values[1:])
+
+        elif values[0] == 'tags':
+            note.clear_tags()
+            return note.add_to_note(values[1:])
+
+        else:
+            return "This field don't exist."
+
+    def edit_name(self, name: str, info: list):
+        """Змінює ім'я запису. Змінює як ім'я-ключ нотатки, так і в самій нотатці"""
+        try:
+            new_name = info[0]
+        except IndexError:
+            raise IndexError(f"Please enter 'old_name' and 'new_name'")
+        record: Note = self.note_book[name]
+        record.name = new_name
+        self.note_book[new_name] = record
+        del self.note_book[name]
+        return f"{name}'s name has been changed to {new_name}"
+
+    def search_in(self, search_data: str, *_) -> list:
+        """Робить пошук у книзі по одному слову"""
+        result = []
+        for value in self.note_book.values():
+            if search_data in (value.name, *value.tags, *value.text.split()):
+                result.append(value)
+        return result
+
+    def sorted_by_tags(self, *args) -> list:
+        """Робить сортування по тегам. Рахує співпадіння тегів, а потім сортує"""
+        tags = [args[0], *args[1]]
+        matched_records = []
+
+        for note in self.note_book.data.values():
+            matched_tags = 0
+            for tag in note.tags:
+                if tag in tags:
+                    matched_tags += 1
+            matched_records.append(f'Matches:{matched_tags}\n'
+                                   f'{str(note)}') if matched_tags else None
+        return sorted(matched_records, reverse=True) if matched_records else "Matches not found"
+
+    def add_values(self, name: str, info: list):
+        """Додає інформацію в нотатку"""
+        note: Note = self.note_book[name]
+        note.add_to_note(info)
+        return f"Value is added to {name}"
